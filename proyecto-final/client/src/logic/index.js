438f4aaac1955e46a23882c95ce87e84a29eba09
tobalo1986 <116591480+tobalo1986@users.noreply.github.com>
2025-12-14 17:43:39 +0100

    feat:cristobalfeat3: modificando rutas para despliegue5

--- proyecto-final/client/src/logic/index.js
+++
@@ -1,4 +0,0 @@
-export { default as getGames } from "./getGames";
-export { default as createGames } from "./createGame";
-export { default as updateGeme } from "./updateGame";
-export { default as deleteGame } from "./deleteGame";
